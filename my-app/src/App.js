f55622c53bed96fae3bb9c8a7fbaada3e535610f
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-02-09 19:55:43 +0530

    add listing_page.js as import in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+// room listing_page import
+import listing_page from './room_listing/listing_page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <listing_page/>
     </div>
   );
 }
