cd6da03c7f1699dabc6142168b4bf22d8bed265c
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-02-10 11:46:35 +0530

    import roomdata.json file to listing page

--- my-app/src/room_listing/listing_page.js
+++ my-app/src/room_listing/listing_page.js
@@ -8,67 +8,74 @@ import CardContent from "@mui/material/CardContent";
 //Import the Room slider js 
 import RoomSlider from '../components/room_slider';
 
+// Import the data from roomdata.json
+import roomobject from '../components/roomdata.json';
+
 function listing_page(){
     return(
 
-        // Master Container
-        <Box sx={{
-            display:'flex',
-            flexDirection:{xs:'column', md:'row'}, // the responsiveness is made for the boxes the left and right
-            bgcolor:'#F1F5F9',
-            color:'#020617',
-            p:2, // the spacing between the left and right boxes are given
-            minHeight:'100vh'
-        }}>
-
-            {/* Left side Box */}
+        <div>
+            {/* Master Container */}
             <Box sx={{
                 display:'flex',
-                flexDirection:'column', // the items inside the box will be like a column
-                flex:1,
-                pt:1, // Padding in all direction are given
-                pl:1,
-                pr:1,
-                pb:1,
-                width:'100%',
-                gap:1
+                flexDirection:{xs:'column', md:'row'}, // the responsiveness is made for the boxes the left and right
+                bgcolor:'#F1F5F9',
+                color:'#020617',
+                p:2, // the spacing between the left and right boxes are given
+                minHeight:'100vh'
             }}>
 
-                {/* Room Slider Component */}
+                {/* Left side Box */}
                 <Box sx={{
                     display:'flex',
-                    flexDirection:'column',
-                    gap:1,
-                    alignItems:'center' // center the card in the column
+                    flexDirection:'column', // the items inside the box will be like a column
+                    flex:1,
+                    pt:1, // Padding in all direction are given
+                    pl:1,
+                    pr:1,
+                    pb:1,
+                    width:'100%',
+                    gap:1
                 }}>
 
-                    <Card varient ="outlined" 
-                    sx={{
-                        bgcolor:'#fffff',
-                        borderRadius:1.5,
-                        border: '1px solid #CBD5E1',
-                        boxShadow: '0 1px 2px rgba(2, 6, 23, 0.08)',
-                        maxWidth: '100%', 
-                        height: 300,
+                    {/* Room Slider Component */}
+                    <Box sx={{
+                        display:'flex',
+                        flexDirection:'column',
+                        gap:1,
+                        alignItems:'center' // center the card in the column
                     }}>
-                        <CardContent sx={{p:0, '&last-child':{pb:0}, height:'100%'}}>
-                            <RoomSlider/>
-                        </CardContent>
 
-                    </Card>
+                        <Card varient ="outlined" 
+                        sx={{
+                            bgcolor:'#fffff',
+                            borderRadius:1.5,
+                            border: '1px solid #CBD5E1',
+                            boxShadow: '0 1px 2px rgba(2, 6, 23, 0.08)',
+                            maxWidth: '100%', 
+                            height: 300,
+                        }}>
+                            <CardContent sx={{p:0, '&last-child':{pb:0}, height:'100%'}}>
+                                <RoomSlider rooms={roomobject}/>
+                            </CardContent>
+
+                        </Card>
+                    </Box>
+
+                    
+                
                 </Box>
-               
+
+
+
+                {/* Right side Box */}
+                <Box>
+
+                </Box>
+
+
             </Box>
-
-
-
-            {/* Right side Box */}
-            <Box>
-
-            </Box>
-
-
-        </Box>
+        </div>
     )
 }
 
