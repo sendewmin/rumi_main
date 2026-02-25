4731cf314b92d905b860257660ec7462ddb2669c
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-02-25 18:28:52 +0530

    update the spring jpa hibernate to update

--- rumi_backend_v2/src/main/resources/db/migration/V5__create_renter_profile.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS renter_profiles (
-    user_id VARCHAR(128) PRIMARY KEY,
-    location VARCHAR(255),
-    gender ENUM('male','female','other'),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
