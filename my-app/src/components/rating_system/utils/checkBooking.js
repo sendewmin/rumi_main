dc2ceb707bb7ac5e6764fdb8b167e1dd3bac6adb
sendewmin <seniru.20241509@iit.ac.lk>
2026-03-13 17:21:56 +0530

    implemented booking elgibility check

---
+++ my-app/src/components/rating_system/utils/checkBooking.js
@@ -0,0 +1,17 @@
+import { supabase } from "../supabaseClient"
+
+export async function checkBooking(userId, roomId) {
+
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*")
+    .eq("user_id", userId)
+    .eq("room_id", roomId)
+
+  if (error) {
+    console.error("Booking check error:", error)
+    return false
+  }
+
+  return data.length > 0
+}
