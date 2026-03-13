cd45eca339e73aef1d91c3a38493a6cac75fe088
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-03-13 18:59:00 +0530

    add error handling in the post method

--- rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/controller/RoomImageController.java
+++ rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/controller/RoomImageController.java
@@ -10,9 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController  //here we register the class as api endpoint
 @RequestMapping(path = "/api/rooms")  //the parent endpoint is /api/rooms
 @RequiredArgsConstructor
@@ -23,13 +26,27 @@ public class RoomImageController {
 
     @PostMapping(path = "/{room_id}/images")  //here the psot mapping url is room id/ image
     //here we take the room_id as Path Param and store it in the room_id variable
-    public ResponseEntity<?> uploadImages(@PathVariable("room_id") long room_id, @RequestParam("image")List<MultipartFile> images){
+    public ResponseEntity<?> uploadImages(@PathVariable("room_id") long roomId, @RequestParam("image")List<MultipartFile> images){
         try{
-            roomImageService.uploadRoomImages(room_id,images);
-            return new ResponseEntity<>("Room Image Added", HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Image Upload Failed", HttpStatus.INTERNAL_SERVER_ERROR);
+            roomImageService.uploadRoomImages(roomId,images);
+            System.out.println("Controller: "+roomId);
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "Room Image Added"));
         }
+        catch(RuntimeException e){
+            if (e.getMessage().contains("Room not found")) {
+                System.out.println("Controller: "+e.getMessage());
+                System.out.println(ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "Room not found")));
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "Room not found"));
+            }
+            System.out.println("Controller: "+e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Internal server error"));
+        }
+
+        catch (Exception e) {
+            System.out.println("Controller: "+e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Image upload failed"));
+        }
+
     }
 
 
@@ -38,9 +55,11 @@ public class RoomImageController {
     @GetMapping(path = "/{room_id}/images")  //Here this is the Get url
     public ResponseEntity<?> fetchImages(@PathVariable("room_id") long room_id){
         try{
-            List<RoomImageDto> roomImageDtos = roomImageService.fetchRoomImages(room_id);
-            return new ResponseEntity<>(roomImageDtos, HttpStatus.CREATED);
-        } catch (Exception e) {
+            //List<RoomImageDto> roomImageDtos = roomImageService.fetchRoomImages(room_id);
+            return new ResponseEntity<>("Room images fetch for "+room_id, HttpStatus.CREATED);
+        }
+        catch (Exception e) {
+            System.out.println("Controller: "+e.getMessage());
             return new ResponseEntity<>("Image Fetch Failed", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
