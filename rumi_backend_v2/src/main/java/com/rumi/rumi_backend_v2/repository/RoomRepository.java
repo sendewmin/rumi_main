ec2cde5b6b6babfed8eabeebbeaa3ff18ceddeb9
NeanthiranPuspaharan <neandiran2006@gmail.com>
2026-03-13 14:01:32 +0530

    Update RoomRepository.java

--- rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/repository/RoomRepository.java
+++ rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/repository/RoomRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    // Find all rooms posted by a specific landlord
+    
     List<Room> findByLandlordId(Long landlordId);
 
-    // Check if a room belongs to a specific landlord (used for security checks)
+
     boolean existsByIdAndLandlordId(Long id, Long landlordId);
 }
