396a875d1d90a63f4e57204a4e65865deded20bd
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-03-13 11:45:39 +0530

    add create connection bridge with the supabase db for file upload

--- rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/util/SupabaseStorageService.java
+++ rumi_backend_v2/src/main/java/com/rumi/rumi_backend_v2/util/SupabaseStorageService.java
@@ -3,6 +3,11 @@ package com.rumi.rumi_backend_v2.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SupabaseStorageService {
 
     @Value("${SUPABASE_URL}")
@@ -17,9 +22,28 @@ public class SupabaseStorageService {
 
     public String upload(MultipartFile file, String path) {
 
-        String uploadUrl=supabaseUrl + "/storage/v1/object/" + bucket+ "/" + path;
+        try{
+            String uploadUrl=supabaseUrl + "/storage/v1/object/" + bucket+ "/" + path;
+            HttpURLConnection connection = getHttpURLConnection(file, uploadUrl);
+            OutputStream os = connection.getOutputStream(); // Here we get the connection outputStream
+            os.write(file.getBytes());  //here we get the file and get the bytes and write it to the connection outputStream
+
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
 
     }
+
+    private HttpURLConnection getHttpURLConnection(MultipartFile file, String uploadUrl) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(uploadUrl).openConnection();  // This opens the connection for the supabase
+        connection.setRequestMethod("POST");  // here we say the connection as POST
+        connection.setDoOutput(true);  // This allows the data to be sent in the body
+        connection.setRequestProperty("Authorization", "Bearer " + serviceKey); //Here we pass the service key to Supabase connection so it can verify
+        connection.setRequestProperty("Content-Type", file.getContentType());  // Here we say what file type it is .
+        return connection;
+    }
+
 }
