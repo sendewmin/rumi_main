66a9d51a9099277ff91a3937228b7ef1d990febe
sendewmin <seniru.20241509@iit.ac.lk>
2026-03-16 00:46:45 +0530

    Add Font Awesome and modernize homepage layout with new styles and components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,14 @@
 
+
 import './App.css';
-// room listing_page import
-import ListingPage from "./room_listing/listing_page";
-import RoomImageUpload from './components/room_image_upload';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import TenantSignup from './components/TenantSignup';
-import LandlordSignup from './components/LandlordSignup';
-import LandlordPage from './user_roles/page/land_lord';
 import Homepage from './components/Homepage';
+
 
 function App() {
   return (
     <div className="App">
-
-
       <Homepage />
-
-
-      <ListingPage/>
-      {/* <RoomImageUpload/> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandlordPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup/tenant" element={<TenantSignup />} />
-          <Route path="/signup/landlord" element={<LandlordSignup />} />
-          <Route path="/landlord" element={<LandlordPage />} />
-        </Routes>
-      </BrowserRouter>
     </div>
-
   );
 }
 
