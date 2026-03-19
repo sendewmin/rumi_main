c125c0d87fa35f06ff0fc1245c3e89d394be84be
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-03-19 14:25:51 +0530

    remove the test run from ci-cd

--- rumi_backend_v2/src/test/java/com/rumi/rumi_backend_v2/RumiBackendV2ApplicationTests.java
+++ rumi_backend_v2/src/test/java/com/rumi/rumi_backend_v2/RumiBackendV2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(locations = "file:./env.properties")
+
 @SpringBootTest
 class RumiBackendV2ApplicationTests {
 
