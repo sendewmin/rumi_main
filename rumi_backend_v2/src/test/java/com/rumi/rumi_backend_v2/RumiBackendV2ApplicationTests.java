60a1966857fb3436bb7281ca11e39efa913ff86c
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-03-19 14:15:57 +0530

    add db credentials for the test

--- rumi_backend_v2/src/test/java/com/rumi/rumi_backend_v2/RumiBackendV2ApplicationTests.java
+++ rumi_backend_v2/src/test/java/com/rumi/rumi_backend_v2/RumiBackendV2ApplicationTests.java
@@ -2,7 +2,9 @@ package com.rumi.rumi_backend_v2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(locations = "file:./env.properties")
 @SpringBootTest
 class RumiBackendV2ApplicationTests {
 
