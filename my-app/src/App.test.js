61b6a5501861db330b22045e78f54ea26d9ef34d
Kavin Thurairaj <thurairajkavin@gmail.com>
2026-03-20 12:12:53 +0530

    add jest config for swiper js

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,4 +1,25 @@
 import { render, screen } from '@testing-library/react';
+
+// This code is the help the jest to help understand the swiper js code and modules.
+jest.mock('swiper/react', () => ({
+  Swiper: ({ children }) => <div data-testid="swiper-mock">{children}</div>,
+  SwiperSlide: ({ children }) => <div data-testid="swiper-slide-mock">{children}</div>,
+}));
+
+jest.mock('swiper/modules', () => ({
+  Navigation: () => null,
+  Pagination: () => null,
+  Scrollbar: () => null,
+  A11y: () => null,
+}));
+
+// Mock all possible Swiper CSS paths
+jest.mock('swiper/css', () => ({}));
+jest.mock('swiper/css/navigation', () => ({}));
+jest.mock('swiper/css/pagination', () => ({}));
+jest.mock('swiper/css/scrollbar', () => ({}));
+
+
 import App from './App';
 
 test('renders learn react link', () => {
